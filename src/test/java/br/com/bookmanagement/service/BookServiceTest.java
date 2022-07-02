f947946af043a23df718aae8f5959130d0605f9f
reinaldoneves <reinaldo_neves@hotmail.com>
2022-07-02 20:00:56 -0300

    Up and running - Implementing tests

---
+++ src/test/java/br/com/bookmanagement/service/BookServiceTest.java
@@ -0,0 +1,49 @@
+package br.com.bookmanagement.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getEntityById() {
+    }
+
+    @Test
+    void getEntityByParameter() {
+    }
+
+    @Test
+    void getBookByIsbn() {
+    }
+
+    @Test
+    void getAllEntities() {
+    }
+
+    @Test
+    void getAllBooksAvailable() {
+    }
+
+    @Test
+    void getAllEntitiesByParameter() {
+    }
+
+    @Test
+    void createEntity() {
+    }
+
+    @Test
+    void updateEntity() {
+    }
+
+    @Test
+    void deleteEntity() {
+    }
+}
